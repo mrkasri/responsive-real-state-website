8fd2a33992cd8883dd3835adfd65597c1420931a
mrkasri <mohammed.kasri1@gmail.com>
2022-08-10 17:54:01 +0100

    add accordions

--- js/main.js
+++ js/main.js
@@ -13,9 +13,44 @@ window.addEventListener('scroll',scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
 
+var swiperPoular= new Swiper(".popular_container",{
+    spaceBetween: 32,
+    grabCursor:true,
+    centeredSlides : true,
+    slidesPerView :'auto',
+    loop:true,
+    navigation :{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    }
+});
+
 
 /*=============== VALUE ACCORDION ===============*/
 
+const accordionItems = document.querySelectorAll('.value_accordion_item');
+
+accordionItems.forEach((item)=>{
+    const accordionHeader = item.querySelector('.value_accordion_header')
+
+    accordionHeader.addEventListener('click', ()=>{
+        const openItem = document.querySelector('.accordion_open')
+        toggleItem(item);
+        if(openItem && openItem!==item) toggleItem(openItem)
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.value_accordion_content')
+    if(item.classList.contains('accordion_open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion_open')
+    }else{
+
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    item.classList.add('accordion_open');
+    }
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
