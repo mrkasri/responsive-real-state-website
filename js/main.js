40b24a16310e4b88ce2eea0b2944159925e6a035
mrkasri <mohammed.kasri1@gmail.com>
2022-08-11 17:15:15 +0100

    add breakpoints

--- js/main.js
+++ js/main.js
@@ -54,11 +54,63 @@ const toggleItem = (item) =>{
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop  = current.offsetTop - 58,
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId +']').classList.add('active_link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId +']').classList.remove('active_link')
+        }
+    })
+}
+
+
+window.addEventListener('scroll',scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+
+function scrollUp(){
+    const scroll_up = document.getElementById('scroll-up')
+    if(this.scrollY >= 350) scroll_up.classList.add('show_scroll')
+    else scroll_up.classList.remove('show_scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
+const themeButton = document.getElementById('theme_button')
+const darkTheme = 'dark_theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark':'light';
+
+const getCurrentIcon= () => document.body.classList.contains(iconTheme) ? 'bx bx-moon':'bx bx-sun'
+
+if (selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'] (darkTheme)
+themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add':'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', ()=>{
+document.body.classList.toggle(darkTheme)
+themeButton.classList.toggle(iconTheme)
+
+localStorage.setItem('selected-theme', getCurrentTheme())
+localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
